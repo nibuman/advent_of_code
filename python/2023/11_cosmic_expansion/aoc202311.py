0e160662b6d9f1331c08e6c438295935420b75f5
Nick Bushby <nick.bushby5@gmail.com>
2023-12-13 20:20:25 +0000

    2023_11 part 2 added

--- python/2023/11_cosmic_expansion/aoc202311.py
+++ python/2023/11_cosmic_expansion/aoc202311.py
@@ -14,55 +14,50 @@ def parse(puzzle_input):
 
 def part1(data: list[list[str]]):
     """Solve part 1."""
-    empty_rows = find_empty_rows(data)
-    empty_cols = find_empty_cols(data)
-    galaxies = find_galaxies(data)
-    inflate_space(data, empty_rows, empty_cols, galaxies)
-    galaxy_combinations = [(g1, g2) for g1 in range(len(galaxies)) for g2 in range(g1+1, len(galaxies))]
-    return sum(manhattan(galaxies[g1], galaxies[g2]) for g1, g2 in galaxy_combinations)
-    
-def find_empty_rows(data) -> set[int]:
-    empty_space = set()
-    for r, row in enumerate(data):
-        if all(char == "." for char in row):
-            empty_space.add(r)
-    return empty_space
-
-def find_empty_cols(data) -> set[int]:
-    empty_cols=  set()
-    for c in range(len(data[0])):
-        if all(row[c] == "." for row in data):
-            empty_cols.add(c)
-    return empty_cols
-
-def inflate_space(data, empty_rows, empty_cols, galaxies, inflation_factor=1):
-    for galaxy in galaxies:
-        galaxy.row += sum(empty_row < galaxy.row for empty_row in empty_rows) * inflation_factor
-        galaxy.col += sum(empty_col < galaxy.col for empty_col in empty_cols) * inflation_factor
-        
-    # EMPTY_ROW = ["."] * len(data[0])
-    # for r in range(len(data), -1, -1):
-    #     if r in empty_rows:
-    #         data.insert(r, EMPTY_ROW)
-    # for c in range(len(data[0]), -1, -1):
-    #     if c in empty_cols:
-    #         for row in data:
-    #             row.insert(c, ".")
-                   
-
-def find_galaxies(data) -> list[P]:
-    galaxies = []
-    for r, row in enumerate(data):
-        for c, char in enumerate(row):
-            if char == "#":
-                galaxies.append(P(r,c))
-    return galaxies
-
-    
+    return calc_distances(data, inflation_factor=2)
 
 
 def part2(data):
     """Solve part 2."""
+    return calc_distances(data, inflation_factor=1_000_000)
+
+
+def calc_distances(data, inflation_factor):
+    empty_rows = find_empty_rows(data)
+    empty_cols = find_empty_cols(data)
+    galaxies = find_galaxies(data)
+    inflate_space(empty_rows, empty_cols, galaxies, inflation_factor)
+    galaxy_combinations = [
+        (g1, g2) for g1 in range(len(galaxies)) for g2 in range(g1 + 1, len(galaxies))
+    ]
+    return sum(manhattan(galaxies[g1], galaxies[g2]) for g1, g2 in galaxy_combinations)
+
+
+def find_empty_rows(data) -> set[int]:
+    return {r for r, row in enumerate(data) if all(char == "." for char in row)}
+
+
+def find_empty_cols(data) -> set[int]:
+    return {c for c in range(len(data[0])) if all(row[c] == "." for row in data)}
+
+
+def inflate_space(empty_rows, empty_cols, galaxies, inflation_factor):
+    for galaxy in galaxies:
+        galaxy.row += sum(empty_row < galaxy.row for empty_row in empty_rows) * (
+            inflation_factor - 1
+        )
+        galaxy.col += sum(empty_col < galaxy.col for empty_col in empty_cols) * (
+            inflation_factor - 1
+        )
+
+
+def find_galaxies(data) -> list[P]:
+    return [
+        P(r, c)
+        for r, row in enumerate(data)
+        for c, char in enumerate(row)
+        if char == "#"
+    ]
 
 
 def solve(puzzle_input):
@@ -71,9 +66,11 @@ def solve(puzzle_input):
     yield "Part 1", part1(data)
     yield "Part 2", part2(data)
 
+
 def read_file(file_name):
     PUZZLE_DIR = pathlib.Path(__file__).parent
     return pathlib.Path(PUZZLE_DIR / file_name).read_text().rstrip().split("\n")
+
 
 if __name__ == "__main__":
     DEFAULT_INPUT_FILES = ["example1.txt", "input.txt"]
--- python/2023/11_cosmic_expansion/test_aoc202311.py
+++ python/2023/11_cosmic_expansion/test_aoc202311.py
@@ -16,6 +16,7 @@ def example2():
     puzzle_input = aoc202311.read_file("example2.txt")
     return aoc202311.parse(puzzle_input)
 
+
 @pytest.fixture
 def real_data():
     puzzle_input = aoc202311.read_file("input.txt")
@@ -45,13 +46,14 @@ def test_part2_example2(example2):
     """Test part 2 on example input."""
     assert aoc202311.part2(example2) == ...
 
+
 # @pytest.mark.skip(reason="Not implemented")
 def test_part1_real(real_data):
     """Test part 1 on real input."""
     assert aoc202311.part1(real_data) == 10289334
 
 
-@pytest.mark.skip(reason="Not implemented")
+# @pytest.mark.skip(reason="Not implemented")
 def test_part2_real(real_data):
     """Test part 2 on real input."""
-    assert aoc202311.part2(real_data) == ...
+    assert aoc202311.part2(real_data) == 649862989626
