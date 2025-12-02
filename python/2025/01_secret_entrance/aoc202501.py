ffeafbe44b7d75ff5041bcaa9d0a4681faecf945
Nick Bushby <nick.bushby5@gmail.com>
2025-12-02 23:33:28 +0000

    2025-01 ruff format

--- python/2025/01_secret_entrance/aoc202501.py
+++ python/2025/01_secret_entrance/aoc202501.py
@@ -4,14 +4,17 @@
 import pathlib
 import sys
 from typing import Generator
+
 START_POS = 50
+
 
 def parse(puzzle_input):
     """Parse input."""
     return puzzle_input
 
+
 def rotations(data) -> Generator[tuple[int, int, int]]:
-    pos = START_POS   
+    pos = START_POS
     for d in data:
         direction = d[0]
         distance = int(d[1:])
@@ -21,8 +24,9 @@ def rotations(data) -> Generator[tuple[int, int, int]]:
         pos = shift % 100
         yield pos, shift, distance
 
+
 def part1(data):
-    return sum(pos==0 for pos, _, _ in rotations(data))
+    return sum(pos == 0 for pos, _, _ in rotations(data))
 
 
 def part2(data):
@@ -32,12 +36,12 @@ def part2(data):
     for pos, shift, distance in rotations(data):
         if distance == 0:
             continue
-        if (old_pos !=0) and (shift <= 0):
-            counts += abs((shift-1)//100)
+        if (old_pos != 0) and (shift <= 0):
+            counts += abs((shift - 1) // 100)
         else:
             counts += abs(shift) // 100
         old_pos = pos
-    return counts             
+    return counts
 
 
 def solve(puzzle_input):
@@ -46,9 +50,11 @@ def solve(puzzle_input):
     yield "Part 1", part1(data)
     yield "Part 2", part2(data)
 
+
 def read_file(file_name):
     PUZZLE_DIR = pathlib.Path(__file__).parent
     return pathlib.Path(PUZZLE_DIR / file_name).read_text().rstrip().split("\n")
+
 
 if __name__ == "__main__":
     DEFAULT_INPUT_FILES = ["example1.txt", "input.txt"]
