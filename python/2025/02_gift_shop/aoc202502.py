d95701c2e68eaeb777d2481df585542e730f75a9
Nick Bushby <nick.bushby5@gmail.com>
2025-12-03 20:03:57 +0000

    2025-02 accidental ruff format on .txt input files, repaired

--- python/2025/02_gift_shop/aoc202502.py
+++ python/2025/02_gift_shop/aoc202502.py
@@ -33,7 +33,16 @@ def part1(data: str):
 
 def part2(data):
     """Solve part 2."""
-
+    invalid_IDs: list[int] = []
+    for start, end in ID_ranges(data):
+        for prod_id in range(start, end + 1):
+            prod_string = str(prod_id)
+            if len(prod_string) % 2 == 1:
+                continue
+            mid = len(prod_string) // 2
+            if prod_string[:mid] == prod_string[mid:]:
+                invalid_IDs.append(prod_id)
+    return sum(invalid_IDs)
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
