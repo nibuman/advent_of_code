d397891c10e3e0526ad65c20c2ef9adc1b3ae5b6
Nick Bushby <nick.bushby5@gmail.com>
2025-12-03 22:21:23 +0000

    2025-03 set up

---
+++ python/2025/03_lobby/README.md
@@ -0,0 +1,5 @@
+# Lobby
+
+**Advent of Code: Day 3, 2025**
+
+See https://adventofcode.com/2025/day/3
---
+++ python/2025/03_lobby/aoc202503.py
@@ -0,0 +1,36 @@
+"""AoC 3, 2025: Lobby."""
+
+# Standard library imports
+import pathlib
+import sys
+
+
+def parse(puzzle_input):
+    """Parse input."""
+
+
+def part1(data):
+    """Solve part 1."""
+
+
+def part2(data):
+    """Solve part 2."""
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    data = parse(puzzle_input)
+    yield "Part 1", part1(data)
+    yield "Part 2", part2(data)
+
+def read_file(file_name):
+    PUZZLE_DIR = pathlib.Path(__file__).parent
+    return pathlib.Path(PUZZLE_DIR / file_name).read_text().rstrip().split("\n")
+
+if __name__ == "__main__":
+    DEFAULT_INPUT_FILES = ["example1.txt", "input.txt"]
+    puzzle_input_files = sys.argv[1:] or DEFAULT_INPUT_FILES
+    for file_name in puzzle_input_files:
+        print(f"\n{file_name}:")
+        solutions = solve(puzzle_input=read_file(file_name))
+        print("\n".join(f"{puzzle}: {solution}" for puzzle, solution in solutions))
