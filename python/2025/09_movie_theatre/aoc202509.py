67e2bfa6fa900ecd6a19772f79ac7cbf9f6dece8
Nick Bushby <nick.bushby5@gmail.com>
2025-12-20 22:58:26 +0000

    2025-09 part 1 solved

--- python/2025/09_movie_theatre/aoc202509.py
+++ python/2025/09_movie_theatre/aoc202509.py
@@ -1,18 +1,79 @@
 """AoC 9, 2025: Movie Theatre."""
 
 # Standard library imports
+from dataclasses import dataclass, field
+import functools
 import pathlib
 import sys
 from datetime import datetime
+import numpy as np
+import math
+import itertools
 
-def parse(puzzle_input: str) -> list[str]:
+
+@dataclass(unsafe_hash=True)
+class V:
+    x: int = field(compare=True, hash=True)
+    y: int = field(compare=True, hash=True)
+
+    @property
+    def dist(self):
+        return math.hypot(self.x, self.y)
+
+    @property
+    def area(self):
+        return (abs(self.x) + 1) * (abs(self.y) + 1)
+
+    @property
+    def _np(self) -> np.ndarray:
+        return np.array([self.x, self.y])
+
+    def __add__(self, other: V) -> V:
+        return V(x=(self.x + other.x), y=(self.y + other.y))
+
+    def __sub__(self, other: V) -> V:
+        return V(x=(self.x - other.x), y=(self.y - other.y))
+
+    def __matmul__(self, other: V):
+        return self._np @ other._np
+
+    @classmethod
+    def from_tuple(cls, vector: tuple[int | str, int | str]) -> V:
+        return cls(x=int(vector[0]), y=int(vector[1]))
+
+
+def parse(puzzle_input: str) -> set[V]:
     """Parse input."""
     data = puzzle_input.split("\n")
-    return data
+    return {V.from_tuple(tuple(row.split(","))) for row in data}
 
 
-def part1(data):
+def calculate_angle(vec1: V, vec2: V) -> float:
+    angle_rad = math.acos((vec1 @ vec2) / (vec1.dist * vec2.dist))
+    return math.degrees(angle_rad)
+
+
+def convex_hull(data: set[V]) -> list[V]:
+    first_point = min(data, key=lambda v: (v.x, v.y))
+    v1 = V(x=0, y=1)
+    convex_hull = [first_point]
+    p1 = first_point
+    while True:
+        p3 = min(data, key=lambda p2: calculate_angle(v1, p2 - p1))
+        if p3 == first_point:
+            return convex_hull
+        v1 = p3 - p1
+        p1 = p3
+        data.remove(p1)
+        convex_hull.append(p1)
+    raise ValueError()
+
+
+def part1(data: set[V]):
     """Solve part 1."""
+    cvh = convex_hull(data)
+    all_vecs = (v2 - v1 for v1, v2 in itertools.combinations(cvh, 2))
+    return max(v.area for v in all_vecs)
 
 
 def part2(data):
@@ -28,9 +89,11 @@ def solve(puzzle_input):
         t2 = datetime.now()
         yield name, result, (t2 - t1).microseconds
 
+
 def read_file(file_name) -> str:
     PUZZLE_DIR = pathlib.Path(__file__).parent
     return pathlib.Path(PUZZLE_DIR / file_name).read_text().rstrip()
+
 
 if __name__ == "__main__":
     DEFAULT_INPUT_FILES = ["example1.txt", "input.txt"]
--- python/2025/09_movie_theatre/test_aoc202509.py
+++ python/2025/09_movie_theatre/test_aoc202509.py
@@ -3,6 +3,7 @@
 # Third party imports
 import aoc202509
 import pytest
+from aoc202509 import V
 
 
 @pytest.fixture
@@ -17,16 +18,43 @@ def real_data():
     return aoc202509.parse(puzzle_input)
 
 
+@pytest.mark.parametrize(
+    ["v1", "v2", "expected_result"],
+    [
+        ((2, 2), (0, 1), 45),
+        ((0, 1), (1, 0), 90),
+        ((0, 1), (0, 1), 0),
+    ],
+)
+def test_calculate_angle(v1, v2, expected_result):
+    v1 = V.from_tuple(v1)
+    v2 = V.from_tuple(v2)
+    result = aoc202509.calculate_angle(v1, v2)
+    assert expected_result == pytest.approx(result)
+
+
+def test_convex_hull(example1):
+    expected_result = [
+        V(x=2, y=3),
+        V(x=2, y=5),
+        V(x=9, y=7),
+        V(x=11, y=7),
+        V(x=11, y=1),
+        V(x=7, y=1),
+    ]
+    result = aoc202509.convex_hull(example1)
+    assert all(expected == actual for expected, actual in zip(expected_result, result))
+
+
 @pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1):
     """Test that input is parsed properly."""
     assert example1 == ...
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
-    assert aoc202509.part1(example1) == ...
+    assert aoc202509.part1(example1) == 50
 
 
 @pytest.mark.skip(reason="Not implemented")
@@ -35,10 +63,9 @@ def test_part2_example1(example1):
     assert aoc202509.part2(example1) == ...
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_real(real_data):
     """Test part 1 on real input."""
-    assert aoc202509.part1(real_data) == ...
+    assert aoc202509.part1(real_data) == 4735268538
 
 
 @pytest.mark.skip(reason="Not implemented")
