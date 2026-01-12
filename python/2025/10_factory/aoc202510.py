0e7e23e89d7ab1768c432605b738fd7e8686edf1
Nick Bushby <nick.bushby5@gmail.com>
2026-01-12 19:53:27 +0000

    2025-10 part 1 complete

--- python/2025/10_factory/aoc202510.py
+++ python/2025/10_factory/aoc202510.py
@@ -4,15 +4,74 @@
 import pathlib
 import sys
 from datetime import datetime
+from dataclasses import dataclass
+import re
+import itertools
+from typing import Iterable
 
-def parse(puzzle_input: str) -> list[str]:
+
+@dataclass
+class Machine:
+    light_positions: int
+    switches_positions: list[int]
+    jolts: list[int]
+
+    @classmethod
+    def from_row(cls, row: str) -> Machine:
+        pattern = re.compile(
+            pattern=r"\[(?P<lights>[\.#]+)\] (?P<switches>\([\(\) \d\,]+) {(?P<jolts>[\d\,]+)}"
+        )
+        row_match = re.match(pattern, row)
+        assert row_match
+        lights = Machine.parse_lights(row_match.group("lights"))
+        switches_positions = Machine.parse_switches(row_match.group("switches"))
+        jolts = [int(c) for c in row_match.group("jolts").split(",")]
+        return Machine(
+            light_positions=lights, switches_positions=switches_positions, jolts=jolts
+        )
+
+    @staticmethod
+    def parse_lights(lights: str) -> int:
+        light = 0
+        for i, char in enumerate(lights):
+            if char == "#":
+                light |= 1 << i
+        return light
+
+    @staticmethod
+    def parse_switches(switches: str) -> list[int]:
+        switch_positions = []
+        for switch in switches.split(" "):
+            switch = switch.removeprefix("(").removesuffix(")")
+            switch_positions.append(sum(1 << int(pos) for pos in switch.split(",")))
+        return switch_positions
+
+
+def parse(puzzle_input: str) -> list[Machine]:
     """Parse input."""
-    data = puzzle_input.split("\n")
-    return data
+    return [Machine.from_row(row) for row in puzzle_input.split("\n")]
 
 
-def part1(data):
+def part1(machines: list[Machine]):
     """Solve part 1."""
+    switch_presses = []
+    for machine in machines:
+        for total_presses in range(len(machine.switches_positions) + 1):
+            presses = itertools.combinations(machine.switches_positions, total_presses)
+            # breakpoint()
+            if any(
+                press_switches(press) == machine.light_positions for press in presses
+            ):
+                switch_presses.append(total_presses)
+                break
+    return sum(switch_presses)
+
+
+def press_switches(presses: Iterable[int]) -> int:
+    lights = 0
+    for press in presses:
+        lights ^= press
+    return lights
 
 
 def part2(data):
@@ -28,9 +87,11 @@ def solve(puzzle_input):
         t2 = datetime.now()
         yield name, result, (t2 - t1).microseconds
 
+
 def read_file(file_name) -> str:
     PUZZLE_DIR = pathlib.Path(__file__).parent
     return pathlib.Path(PUZZLE_DIR / file_name).read_text().rstrip()
+
 
 if __name__ == "__main__":
     DEFAULT_INPUT_FILES = ["example1.txt", "input.txt"]
--- python/2025/10_factory/test_aoc202510.py
+++ python/2025/10_factory/test_aoc202510.py
@@ -17,16 +17,46 @@ def real_data():
     return aoc202510.parse(puzzle_input)
 
 
+@pytest.mark.parametrize(
+    ["input", "expected_result"],
+    [(".##.", 6), ("#..##", 25)],
+)
+def test_parse_lights(input: str, expected_result: int):
+    result = aoc202510.Machine.parse_lights(input)
+    assert result == expected_result
+
+
+@pytest.mark.parametrize(
+    ["input", "expected_result"],
+    [
+        ("(3) (1,3) (2) (2,3) (0,2) (0,1)", [8, 10, 4, 12, 5, 3]),
+    ],
+)
+def test_parse_switches(input: str, expected_result: int):
+    result = aoc202510.Machine.parse_switches(input)
+    assert result == expected_result
+
+
+@pytest.mark.parametrize(
+    ["input", "expected_result"],
+    [
+        ([8, 10, 4], 6),
+    ],
+)
+def test_press_switches(input: list[int], expected_result: int):
+    result = aoc202510.press_switches(input)
+    assert result == expected_result
+
+
 @pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1):
     """Test that input is parsed properly."""
     assert example1 == ...
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
-    assert aoc202510.part1(example1) == ...
+    assert aoc202510.part1(example1) == 7
 
 
 @pytest.mark.skip(reason="Not implemented")
@@ -35,10 +65,9 @@ def test_part2_example1(example1):
     assert aoc202510.part2(example1) == ...
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_real(real_data):
     """Test part 1 on real input."""
-    assert aoc202510.part1(real_data) == ...
+    assert aoc202510.part1(real_data) == 530
 
 
 @pytest.mark.skip(reason="Not implemented")
